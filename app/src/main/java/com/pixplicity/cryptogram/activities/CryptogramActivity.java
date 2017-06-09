fa51786031723bd79f757874a0bf96791b201e78
Paul Lammertsma <pflammertsma@gmail.com>
2017-06-10 00:45:56 +0200

    resolved #21

--- app/src/main/java/com/pixplicity/cryptogram/activities/CryptogramActivity.java
+++ app/src/main/java/com/pixplicity/cryptogram/activities/CryptogramActivity.java
@@ -288,6 +288,8 @@ public class CryptogramActivity extends BaseActivity implements GoogleApiClient.
         } else {
             onGameplayReady();
         }
+
+        mHintView.setVisibility(PrefsUtils.getShowHints() ? View.VISIBLE : View.GONE);
     }
 
     @Override
