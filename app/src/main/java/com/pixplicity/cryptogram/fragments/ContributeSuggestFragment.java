30ac99edca10e679019bccdf2b6304ba12ac3182
Paul Lammertsma <pflammertsma@gmail.com>
2017-07-11 22:33:47 +0200

    working on languages

--- app/src/main/java/com/pixplicity/cryptogram/fragments/ContributeSuggestFragment.java
+++ app/src/main/java/com/pixplicity/cryptogram/fragments/ContributeSuggestFragment.java
@@ -1,29 +1,39 @@
 package com.pixplicity.cryptogram.fragments;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.pixplicity.cryptogram.R;
+import com.pixplicity.cryptogram.adapters.SimpleAdapter;
 import com.pixplicity.cryptogram.events.SubmissionEvent;
 import com.pixplicity.cryptogram.models.Puzzle;
 import com.pixplicity.cryptogram.utils.Database;
 import com.pixplicity.cryptogram.utils.EventProvider;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 
 import butterknife.BindView;
@@ -48,6 +58,9 @@ public class ContributeSuggestFragment extends BaseFragment {
     @BindView(R.id.cb_explicit)
     protected CheckBox mCbExplicit;
 
+    @BindView(R.id.sp_language)
+    protected Spinner mSpLanguage;
+
     public static Fragment create() {
         return new ContributeSuggestFragment();
     }
@@ -67,10 +80,56 @@ public class ContributeSuggestFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // TODO apply languages
-        for (Locale locale : Locale.getAvailableLocales()) {
-            Log.d(TAG, locale.getLanguage() + "_" + locale.getCountry() + " [" + locale.getDisplayName() + "]");
-        }
+
+        List<Locale> locales = new ArrayList<>();
+        locales.add(Locale.getDefault());
+        final SimpleAdapter<Locale> adapter = new SimpleAdapter<Locale>(locales) {
+            @Override
+            public String getText(int position) {
+                return getItem(position).getDisplayName();
+            }
+        };
+        mSpLanguage.setAdapter(adapter);
+        mSpLanguage.setSelection(0);
+        new AsyncTask<Void, Void, List<Locale>>() {
+            public int mPosition;
+            Comparator<Locale> mComparator = new Comparator<Locale>() {
+                @Override
+                public int compare(Locale l1, Locale l2) {
+                    return l1.getDisplayName().compareTo(l2.getDisplayName());
+                }
+            };
+
+            @Override
+            protected List<Locale> doInBackground(Void... voids) {
+                List<Locale> locales = Arrays.asList(Locale.getAvailableLocales());
+                Collections.sort(locales, mComparator);
+                mPosition = Collections.binarySearch(locales, Locale.getDefault(), mComparator);
+                return locales;
+            }
+
+            @Override
+            protected void onPostExecute(List<Locale> locales) {
+                adapter.setItems(locales);
+                mSpLanguage.setSelection(mPosition);
+            }
+        }.execute();
+        mSpLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                Locale item = adapter.getItem(position);
+                String localeString = item.getLanguage();
+                String country = item.getCountry();
+                if (!TextUtils.isEmpty(country)) {
+                    localeString += "-r" + country;
+                }
+                Toast.makeText(getContext(), localeString, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
 
     @Override
