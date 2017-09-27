5b79d721379914ea75d550861c589cac0fac90da
Paul Lammertsma <pflammertsma@gmail.com>
2017-09-27 18:16:31 +0200

    added more instructions

--- app/src/main/java/com/pixplicity/cryptogram/fragments/HowToPlayFragment.java
+++ app/src/main/java/com/pixplicity/cryptogram/fragments/HowToPlayFragment.java
@@ -1,10 +1,12 @@
 package com.pixplicity.cryptogram.fragments;
 
+import android.graphics.ColorMatrixColorFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.pixplicity.cryptogram.R;
@@ -20,11 +22,31 @@ public class HowToPlayFragment extends BaseFragment {
 
     private static final String TAG = HowToPlayFragment.class.getSimpleName();
 
+    /**
+     * Color matrix that flips the components (<code>-1.0f * c + 255 = 255 - c</code>)
+     * and keeps the alpha intact.
+     */
+    private static final float[] NEGATIVE = {
+            -1.0f,     0,     0,    0, 255, // red
+            0, -1.0f,     0,    0, 255, // green
+            0,     0, -1.0f,    0, 255, // blue
+            0,     0,     0, 1.0f,   0  // alpha
+    };
+
     @BindView(R.id.tv_how_to_play_1)
     protected TextView mTvHowToPlay1;
 
     @BindView(R.id.tv_how_to_play_2)
     protected TextView mTvHowToPlay2;
+
+    @BindView(R.id.tv_how_to_play_3)
+    protected TextView mTvHowToPlay3;
+
+    @BindView(R.id.iv_instructions1)
+    protected ImageView mIvInstructions1;
+
+    @BindView(R.id.iv_instructions2)
+    protected ImageView mIvInstructions2;
 
     @BindView(R.id.rf_video1)
     protected RatioFrameLayout mRfVideo1;
@@ -39,10 +61,17 @@ public class HowToPlayFragment extends BaseFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        if (isDarkTheme()) {
+            mIvInstructions1.getDrawable().setColorFilter(new ColorMatrixColorFilter(NEGATIVE));
+            mIvInstructions2.getDrawable().setColorFilter(new ColorMatrixColorFilter(NEGATIVE));
+        }
+
         mTvHowToPlay1.setText(
                 HtmlCompat.fromHtml(getString(R.string.how_to_play_1)));
         mTvHowToPlay2.setText(
                 HtmlCompat.fromHtml(getString(R.string.how_to_play_2)));
+        mTvHowToPlay3.setText(
+                HtmlCompat.fromHtml(getString(R.string.how_to_play_3)));
 
         VideoUtils.setup(getActivity(), mRfVideo1, VideoUtils.VIDEO_INSTRUCTION);
     }
