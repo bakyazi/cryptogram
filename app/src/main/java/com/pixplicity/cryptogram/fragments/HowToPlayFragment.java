39786fd4729dd69c073931a5835276fab490c097
Paul Lammertsma <pflammertsma@gmail.com>
2017-10-16 15:56:00 +0200

    link to about screen from help

--- app/src/main/java/com/pixplicity/cryptogram/fragments/HowToPlayFragment.java
+++ app/src/main/java/com/pixplicity/cryptogram/fragments/HowToPlayFragment.java
@@ -3,12 +3,16 @@ package com.pixplicity.cryptogram.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.pixplicity.cryptogram.R;
+import com.pixplicity.cryptogram.activities.AboutActivity;
 import com.pixplicity.cryptogram.utils.HtmlCompat;
 import com.pixplicity.cryptogram.utils.VideoUtils;
 
@@ -43,9 +47,21 @@ public class HowToPlayFragment extends BaseFragment {
     protected RatioFrameLayout mRfVideo1;
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_how_to_play, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_how_to_play, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
@@ -67,6 +83,18 @@ public class HowToPlayFragment extends BaseFragment {
                 HtmlCompat.fromHtml(getString(R.string.how_to_play_4)));
 
         VideoUtils.setup(getActivity(), mRfVideo1, VideoUtils.VIDEO_INSTRUCTION);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about: {
+                startActivity(AboutActivity.create(getContext()));
+            }
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
